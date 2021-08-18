bdda05a1206620be39e8a7d8b93641f673e53fa4
Sean Ni <nixiang915@hotmail.com>
2021-08-18 17:41:59 -0400

    refactor test data to array

--- __tests__/data.test.js
+++ __tests__/data.test.js
@@ -1,16 +1,31 @@
 const { parseData } = require("../data");
-const user = {
-  id: 1,
-  name: "Test User",
-  tags: ["a", "b"],
-};
-const job = {
-  id: 1,
-  title: "Test developer",
-  company: "Test industries",
-  tags: ["a", "b", "c"],
-};
-const data = [user, job];
+const users = [
+  {
+    id: 1,
+    name: "Test User",
+    tags: ["a", "b"],
+  },
+  {
+    id: 2,
+    name: "Test User2",
+    tags: ["b", "c"],
+  },
+];
+const jobs = [
+  {
+    id: 1,
+    title: "Test developer",
+    company: "Test industries",
+    tags: ["a", "b", "c"],
+  },
+  {
+    id: 2,
+    title: "Test tester",
+    company: "Test company",
+    tags: ["c", "b", "d"],
+  },
+];
+const data = [...users, ...jobs];
 
 describe("parseData expected to separate user and job data from an array", () => {
   test("Expect error if given incorrect or corrupted data", () => {
@@ -18,24 +33,24 @@ describe("parseData expected to separate user and job data from an array", () =>
   });
 
   test("Expect user data to be extracted", () => {
-    expect(parseData(data).users[0]).toEqual(expect.objectContaining(user));
+    expect(parseData(data).users[0]).toEqual(expect.objectContaining(users[0]));
   });
 
   test("Expect user data to be extracted with extra incoming data fields", () => {
-    expect(parseData([{ ...user, extra: "data" }, job]).users[0]).toEqual(
-      expect.objectContaining(user)
-    );
+    expect(
+      parseData([{ ...users[0], extra: "data" }, ...jobs]).users[0]
+    ).toEqual(expect.objectContaining(users[0]));
   });
 
   test("Expect job data to be extracted", () => {
-    expect(parseData(data).jobs[0]).toEqual(expect.objectContaining(job));
+    expect(parseData(data).jobs[0]).toEqual(expect.objectContaining(jobs[0]));
   });
 
   test("Expect job data to be extracted with extra incoming data fields", () => {
-    expect(parseData([{ ...job, extra: "data" }, user]).jobs[0]).toEqual(
-      expect.objectContaining(job)
-    );
+    expect(
+      parseData([{ ...jobs[0], extra: "data" }, ...users]).jobs[0]
+    ).toEqual(expect.objectContaining(jobs[0]));
   });
 });
 
-describe("form tagTable expected to form a list of unqiue tags and record matching those tags", () => {});
+describe("formTagTable expected to form a list of unique tags and records matching those tags", () => {});
